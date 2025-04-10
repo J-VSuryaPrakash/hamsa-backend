d743dcd29e1b81614064a21c4942f027e192e74f
Surya Prakash <surya.jinagam@gmail.com>
2025-04-10 12:10:01 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+db.connect(err => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err)
+    } else {
+      console.log('Connected to MySQL database!')
+    }
+})
+
+app.post('/api/contact-info', (req, res) => {
+    const {
+      firstName,
+      secondName = null,
+      email,
+      mobile: mobileNo,
+      message = null
+    } = req.body
+  
+    const sql = `INSERT INTO contacts (mobileNo, firstName, secondName, email, message) VALUES (?, ?, ?, ?, ?)`
+  
+    db.query(sql, [mobileNo, firstName, secondName, email, message], (err, result) => {
+      if (err) {
+        console.error('Error inserting data:', err)
+        return res.status(500).json({ message: 'Database insert failed' })
+      }
+      res.status(200).json({ message: 'Contact saved successfully', insertId: result.insertId })
+    })
+  })
+
+app.listen(port,()=>{
+    console.log(`The server is listening at http://localhost:${port}/`)    
+})
